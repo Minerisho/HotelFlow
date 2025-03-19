bac62480ccc107201fcb65733271c810106a1e51
Minerisho <migueplata5@gmail.com>
2025-03-19 09:04:59 -0500

    feat: Primer commit, entorno de desarrollo con sus extensiones respectivas.

---
+++ src/main/java/entornos/hotelflow/hotel_flow/HotelFlowApplication.java
@@ -0,0 +1,13 @@
+package entornos.hotelflow.hotel_flow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/entornos/hotelflow/hotel_flow/HotelFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package entornos.hotelflow.hotel_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
